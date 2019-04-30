250d79d5fbee9dde6211ae0853b4b10aef25bbdc
Claudio Bernasconi <email@claudio-bernasconi.net>
2019-04-30 22:19:50 +0200

    Initial commit

---
+++ OBSCountdown/CountdownDuration.cs
@@ -0,0 +1,50 @@
+﻿namespace OBSCountdown
+{
+    internal class CountdownDuration
+    {
+        public int Hours { get; private set; }
+        public int Minutes { get; private set; }
+        public int Seconds { get; private set; }
+
+        public bool Tick()
+        {
+            if (Seconds == 0 && Minutes == 0 && Hours == 0)
+            {
+                return false;
+            }
+            else if (Minutes == 0 && Seconds == 0)
+            {
+                Hours--;
+                Minutes = 59;
+                Seconds = 60;
+            }
+            else if (Seconds == 0)
+            {
+                Minutes--;
+                Seconds = 60;
+            }
+            Seconds--;
+
+            return true;
+        }
+
+        public string GetOutput()
+        {
+            return Hours.ToString("00") + ":" + Minutes.ToString("00") + ":" + Seconds.ToString("00");
+        }
+
+        internal static CountdownDuration Parse(int secondsToGo)
+        {
+            int hours = secondsToGo / 3600;
+            int minutes = (secondsToGo - hours * 3600) / 60;
+            int seconds = secondsToGo - (minutes * 60) - (hours * 3600);
+
+            return new CountdownDuration
+            {
+                Hours = hours,
+                Minutes = minutes,
+                Seconds = seconds,
+            };            
+        }
+    }
+}
---
+++ OBSCountdown/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Threading;
+
+namespace OBSCountdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secondsToGo = GetSecondsFromArgs(args);
+            var duration = CountdownDuration.Parse(secondsToGo);
+
+            do
+            {
+                Thread.Sleep(990);
+                WriteFile(duration.GetOutput());
+            } while (duration.Tick());
+        }
+
+        private static int GetSecondsFromArgs(string[] args)
+        {
+            return int.Parse(args[args.Length - 1]);
+        }
+
+        private static void WriteFile(string input)
+        {
+            File.WriteAllText("countdown.txt", input);
+        }
+    }
+}
